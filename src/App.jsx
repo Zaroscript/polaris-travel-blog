3b8539c5a0623d2467958c28839d07e0d3778091
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2025-03-07 12:36:42 +0200

    Initial commit
    Upload the project structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Button from "react-bootstrap/Button";
+
+const Home = () => {
+  return <div className=""></div>;
+};
+
+export default Home;
