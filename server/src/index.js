4d26eef87054dd7c988e78932d8eaa23c287d3b6
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2025-03-23 02:02:10 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.VITE_CLIENT_URL,
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
