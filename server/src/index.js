eb481794e5e751b46721dcc13a4f957bcb95b912
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2025-03-31 10:45:51 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
