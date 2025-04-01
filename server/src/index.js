4f2e3e7eb7bf5d177529627a6f09c1559d8b632f
mariam-nabil-dev <mariammeroo510@gmail.com>
2025-04-01 13:29:54 +0200

    fix bugs

--- server/src/index.js
+++ server/src/index.js
@@ -23,8 +23,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
