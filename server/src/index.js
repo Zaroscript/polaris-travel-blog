1a72153eda03e927b2d851268616cdec2d1c22a1
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2025-04-03 23:09:31 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,12 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: *,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+  cors()
 );
 
 app.use("/api/auth", authRoutes);
